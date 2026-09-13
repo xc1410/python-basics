3ad97691d0c28b390f4df710c37453fa4303ebb4
Cc <xc2242017814@gmail.com>
2026-09-13 15:33:03 +0800

    feat: day05 字符串与 set

--- day05.py
+++ day05.py
@@ -43,4 +43,64 @@ print(min_max([3, 1, 4, 1, 5]))        # (1, 5)
 low, high = min_max([3, 1, 4, 1, 5])
 print(low, high)                        # 1 5
 
+#1
+def my_strip(s):
+    left=0
+    while left+1<=len(s) and s[left]==" ":
+        left=left+1
+    right=len(s)-1
+    while right>=0 and s[right]==" ":
+            right=right-1
+    return s[left:right+1]
+
+    
+    
+
+print(my_strip("   hello   "))       # hello
+print(my_strip("  a b  "))           # a b      ← 中间那个空格要留着
+print(my_strip("abc"))               # abc
+print(my_strip("    "))              # 空字符串
+#2
+def my_split(a):
+    result=[]
+    word=""
+    for c in a:
+        if c!=" ":
+            word=word+c
+        else:
+            if word !="":
+                result.append(word)
+                word=""
+    if word !="":
+        result.append(word)
+
+    return result   
+print(my_split("a b c"))          # ['a', 'b', 'c']
+print(my_split("hello world"))    # ['hello', 'world']
+print(my_split("a  b"))           # ['a', 'b']   ← 两个空格，不要空字符串
+
+#3
+def thesame(a,b):
+    result=[]
+    for n1 in a:
+        if n1 in b and n1 not in result:
+            result.append(n1)
+    return result
+a = [1, 2, 2, 3]
+b = [2, 3, 4]
+
+# 1. 用两层循环求交集
+# 2. 用 set 求交集，对比行数
+print(thesame(a,b))
+
+def thesame_set(a, b):
+    s1=set(a)
+    s2=set(b)
+    result=s1 & s2
+    return result
+a = [1, 2, 2, 3]
+b = [2, 3, 4]
+print(thesame_set(a,b))
+
+
     
